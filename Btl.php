94fb4a49d7f07c12d2fa7a07a28742e3ba5a222a
Guillaume Bodi <guillaume@spinifexgroup.com>
2012-08-06 09:55:17 +1000

    Initial commit

---
+++ Btl.php
@@ -0,0 +1,32 @@
+<?php
+namespace application\plugin\btl
+{
+
+	use nutshell\core\plugin\Plugin;
+	use nutshell\behaviour\Native;
+	use nutshell\behaviour\AbstractFactory;
+	use nutshell\Nutshell;
+	
+	/**
+	 * BTL (pron. "Beatle") - The Batchable Transmission Layer.
+	 * Heaps compatible with GSRC
+	 * @author Dean Rather
+	 */
+	class Btl extends Plugin implements Native, AbstractFactory
+	{
+		public static function loadDependencies()
+		{
+			require_once(__DIR__._DS_.'BtlException.php');
+			require_once(__DIR__._DS_.'Service.php');
+		}
+		
+		public static function registerBehaviours(){}
+		
+		public static function runFactory($handler)
+		{	
+			// We might need this if they do a 'query' request
+//			$this->plugin->MvcQuery();
+			return new Service();
+		}
+	}
+}
---
+++ BtlException.php
@@ -0,0 +1,56 @@
+<?php
+namespace application\plugin\btl
+{
+	use nutshell\core\exception\NutshellException;
+
+	/**
+	 * @author Dean Rather
+	 */
+	class BtlException extends NutshellException
+	{
+		/** The request must be valid JSON */
+		const INVALID_REQUEST			= 1;
+		
+		/** See btl\Service\setControllerNamespace() */
+		const MUST_SET_NAMESPACE		= 2;
+		
+		const FILE_DOESNT_EXIST			= 3;
+		const CLASS_DOESNT_EXIST		= 4;
+		const CLASS_METHOD_DOESNT_EXIST	= 5;
+		
+		/** Query must be one of: get, set, remove, check */
+		const INVALID_QUERY_TYPE		= 6;
+		
+		/** The request must contain a 'data' or 'query' part */
+		const REQUEST_NEEDS_DATA		= 7;
+		
+		/** The request must contain a 'sequence' which must be a positive integer */
+		const REQUEST_NEEDS_SEQUENCE	= 8;
+		
+		/** The request must contain a 'timestamp' which must be a positive integer */
+		const REQUEST_NEEDS_TIMESTAMP	= 9;
+		
+		/** The request must contain a 'call' */
+		const REQUEST_NEEDS_CALL		= 10;
+		
+		/** Your application's config.json must have a "Btl" section which defined which classes/methods are accessable */
+		const API_CONFIG_NOT_CONFIGURED	= 11;
+		
+		/** The class must be defined in api.json */
+		const API_CLASS_NOT_ALLOWED		= 13;
+		
+		/** The class method must be defined in api.json */
+		const API_CLASS_METHOD_NOT_ALLOWED = 14;
+		
+		/** This feature doesn't exist yet */
+		const TODO = 15;
+
+		/** The "Call" part of the BTL query must contain exactly 1 dot. */
+		const INVALID_CALL_FORMAT = 16;
+		
+		/** The "query" block is only available for "get" methods" **/
+		const CANT_QUERY_THAT_TYPE = 17;
+		
+	}
+}
+?>
